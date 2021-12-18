5401cf5cd7e0f33f235e3c174a0738c38e5e09e0
mustafaboz <mustafaegebozkaya555@hotmail.com>
2021-12-19 00:53:19 +0300

    was developed with addions

--- src/Nav.js
+++ src/Nav.js
@@ -1,87 +1,65 @@
-import React, { Component } from 'react';
-import { Nav,NavbarToggler,DropdownItem,DropdownMenu,DropdownToggle
-     ,Navbar,NavbarBrand,Collapse,
-     NavItem, NavLink,
-    UncontrolledDropdown,NavbarText} from 'reactstrap';
-
-
+import React, { Component } from "react";
+import {
+  Nav,
+  NavbarToggler,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Navbar,
+  NavbarBrand,
+  Collapse,
+  NavItem,
+  NavLink,
+  NavbarText,
+  Button,
+  Dropdown,
+} from "reactstrap";
+import CartList from "./CartList";
 
 export default class Navi extends Component {
-
-  
   constructor(props) {
     super(props);
 
-    this.state={
-      brand :this.props.brand
-    }
-  }
-  
-  changestate=()=>{
-
-    this.setState({brand:"new React Apps"})
-
+    this.state = {
+      brand: this.props.brand,
+    };
   }
 
-  
-    render() {
-        return (
-            <div>
-            <Navbar
-              color="light"
-              expand="md"
-              light
-            >
-              <NavbarBrand  onClick={this.changestate} href="/">
-                {this.state.brand}
-              </NavbarBrand>
-              <NavbarToggler onClick={this.changestate} />
-              <Collapse navbar>
-                <Nav
-                  className="me-auto"
-                  navbar
-                >
-                  <NavItem>
-                    <NavLink href="/components/">
-                      Components
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink href="https://github.com/reactstrap/reactstrap">
-                      GitHub
-                    </NavLink>
-                  </NavItem>
-                  <UncontrolledDropdown
-                    inNavbar
-                    nav
-                  >
-                    <DropdownToggle
-                      caret
-                      nav
-                    >
-                      Options
-                    </DropdownToggle>
-                    <DropdownMenu right>
-                      <DropdownItem>
-                        Option 1
-                      </DropdownItem>
-                      <DropdownItem>
-                        Option 2
-                      </DropdownItem>
-                      <DropdownItem divider />
-                      <DropdownItem>
-                        Reset
-                      </DropdownItem>
-                    </DropdownMenu>
-                  </UncontrolledDropdown>
-                </Nav>
-                <NavbarText>
-                  Simple Text
-                </NavbarText>
-              </Collapse>
-            </Navbar>
-          </div>
-            
-        );
-    }
+  changestate = () => {
+    this.setState({ brand: "E-TRADİNG APP" });
+  };
+
+  render() {
+    return (
+      <div>
+        <Navbar color="warning" expand="md" fixed="false" light>
+          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <NavbarToggler onClick={function noRefCheck() {}} />
+          <Collapse navbar>
+            <Nav className="me-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">
+                  GitHub
+                </NavLink>
+              </NavItem>
+              <CartList carts={this.props.cart} />
+            </Nav>
+            <NavbarText>
+              <Button color="warning">CARTS ({this.props.cart.length})</Button>
+            </NavbarText>
+            <Dropdown toggle={function noRefCheck() {}}>
+              <DropdownToggle color="warning" caret></DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem header>Header</DropdownItem>
+                <DropdownItem>Action</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
 }

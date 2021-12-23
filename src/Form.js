6d46aa1ab7da48b8ff33d74d8e85a9a8d8596479
mustafaboz <mustafaegebozkaya555@hotmail.com>
2021-12-24 00:36:57 +0300

    user form  page added

---
+++ src/Form.js
@@ -0,0 +1,125 @@
+import React, { Component } from "react";
+import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
+
+export default class Formuser extends Component {
+  state = {
+    username: "",
+    email: "",
+    password: "",
+  };
+
+  onchange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  onSubmithandler = (event) => {
+    event.preventDefault();
+    alert(this.state.username);
+  };
+  render() {
+    return (
+      <div className="container">
+        <Form onSubmit={this.onSubmithandler}>
+          <FormGroup>
+            <Label for="exampleUsername">Username--{this.state.username}</Label>
+            <Input
+              onChange={this.onchange}
+              type="text"
+              name="username"
+              id="exampleUsername"
+              placeholder="with a placeholder"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="exampleEmail">Email--{this.state.email}</Label>
+            <Input
+              onChange={this.onchange}
+              type="email"
+              name="email"
+              id="exampleEmail"
+              placeholder="with a placeholder"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="examplePassword">
+              Password --{this.state.password}
+            </Label>
+            <Input
+              onChange={this.onchange}
+              type="password"
+              name="password"
+              id="examplePassword"
+              placeholder="password placeholder"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="exampleSelect">Select</Label>
+            <Input type="select" name="select" id="exampleSelect">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </Input>
+          </FormGroup>
+          <FormGroup>
+            <Label for="exampleSelectMulti">Select Multiple</Label>
+            <Input
+              type="select"
+              name="selectMulti"
+              id="exampleSelectMulti"
+              multiple
+            >
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </Input>
+          </FormGroup>
+          <FormGroup>
+            <Label for="exampleText">Text Area</Label>
+            <Input type="textarea" name="text" id="exampleText" />
+          </FormGroup>
+          <FormGroup>
+            <Label for="exampleFile">File</Label>
+            <Input type="file" name="file" id="exampleFile" />
+            <FormText color="muted">
+              This is some placeholder block-level help text for the above
+              input. It's a bit lighter and easily wraps to a new line.
+            </FormText>
+          </FormGroup>
+          <FormGroup tag="fieldset">
+            <legend>Radio Buttons</legend>
+            <FormGroup check>
+              <Label check>
+                <Input type="radio" name="radio1" /> Option one is this and
+                that—be sure to include why it's great
+              </Label>
+            </FormGroup>
+            <FormGroup check>
+              <Label check>
+                <Input type="radio" name="radio1" /> Option two can be something
+                else and selecting it will deselect option one
+              </Label>
+            </FormGroup>
+            <FormGroup check disabled>
+              <Label check>
+                <Input type="radio" name="radio1" disabled /> Option three is
+                disabled
+              </Label>
+            </FormGroup>
+          </FormGroup>
+          <FormGroup check>
+            <Label check>
+              <Input type="checkbox" /> Check me out
+            </Label>
+          </FormGroup>
+          <Button>Submit</Button>
+        </Form>
+      </div>
+    );
+  }
+}
